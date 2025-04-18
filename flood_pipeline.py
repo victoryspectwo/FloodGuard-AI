4e0d9cea6e136cbcd0641b5c4457d76b84a5dcf5
victoryspectwo <zeenigma462@gmail.com>
2025-04-18 22:24:33 +0800

    Added requirements.txt for easier venv package management

--- flood_pipeline.py
+++ flood_pipeline.py
@@ -42,6 +42,7 @@ def get_forecast_data():
     url = "https://api-open.data.gov.sg/v2/real-time/api/two-hr-forecast"
     response = requests.get(url)
     data = response.json()
+    print(data)
     forecast_map = {
         "Thundery Showers": 2,
         "Showers": 1,
